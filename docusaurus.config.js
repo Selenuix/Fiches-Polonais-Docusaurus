5e9db5bfe049d2d8e314007bda8899a37e7fe728
Selenuix <anthony.ciszek@outlook.com>
2022-01-24 21:35:04 +0100

    Add: Gitguard Auto Scan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ module.exports = {
           blogDescription: 'Un moyen rapide de retrouver mes notes.',
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Selenuix/Fiches-Polonais/edit/master/website/blog/',
+            'https://github.com/Selenuix/Fiches-Polonais-Docusaurus/tree/develop/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
